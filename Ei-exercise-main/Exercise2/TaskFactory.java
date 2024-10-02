7e17fbd6485b8d15283ada87ce0029bb20e2552e
rakeshreddy03 <139332520+rakeshreddy03@users.noreply.github.com>
2024-10-02 16:18:49 +0530

    Delete Ei-exercise-main directory

--- Ei-exercise-main/Exercise2/TaskFactory.java
+++
@@ -1,5 +0,0 @@
-public class TaskFactory {
-    public static Task createTask(String description, String startTime, String endTime, String priority) {
-        return new Task(description, startTime, endTime, priority);
-    }
-}
